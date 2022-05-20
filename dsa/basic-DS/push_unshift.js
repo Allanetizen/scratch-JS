abe815b44a4c2dac44c63606c2070aff7f2f6812
Allanetizen <allankosgei747@gmail.com>
2022-05-20 17:39:13 +0300

    adding element at the begin and end of an array using unshift() and push() respectively

---
+++ dsa/basic-DS/push_unshift.js
@@ -0,0 +1,12 @@
+function mixedNumbers(arr) {
+    // Only change code below this line
+    //unshift
+    arr.unshift('I',2,'three');
+    arr.push(7,'VIII', 9)
+  
+  
+    // Only change code above this line
+    return arr;
+  }
+  
+  console.log(mixedNumbers(['IV', 5, 'six']));
